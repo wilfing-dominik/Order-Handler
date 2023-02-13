b97b6041a401048c7a067df2f2a406d9664b7a5e
wilfing-dominik <wilfing.dominik00@gmail.com>
2023-02-13 15:40:59 +0100

    removed home screen styling, refactored code

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,12 +2,12 @@ import React, {useState, useEffect} from 'react';
 import {useIsFocused} from '@react-navigation/native';
 import {
   Text,
-  View,
   Button,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
-  listViewItemSeparator,
+  StyleSheet,
+  View,
 } from 'react-native';
 import {sqlQuery} from '../utils/dbConnection';
 
@@ -18,6 +18,18 @@ export default HomeScreen = ({navigation}) => {
 
   const isVisible = useIsFocused();
 
+  const calculateTableSum = () => {
+    let temp = new Array(items.length).fill(0);
+    for (let i = 0; i < items.length; i++) {
+      for (let j = 0; j < orders.length; j++) {
+        if (items[i].id == orders[j].localtable_id) {
+          temp[i] += orders[j].huf * orders[j].amount;
+        }
+      }
+    }
+    return temp;
+  };
+
   // SQL GET ALL ORDERS JOINED WITH PRODUCTS
   useEffect(() => {
     if (isVisible) {
@@ -26,15 +38,8 @@ export default HomeScreen = ({navigation}) => {
         setOrders,
       );
 
-      let temp = new Array(items.length).fill(0);
-      for (let i = 0; i < items.length; i++) {
-        for (let j = 0; j < orders.length; j++) {
-          if (items[i].id == orders[j].localtable_id) {
-            temp[i] += orders[j].huf * orders[j].amount;
-          }
-        }
-      }
-      setTableSum(temp);
+      let newTableSum = calculateTableSum();
+      setTableSum(newTableSum);
     }
   }, [isVisible, items]);
 
@@ -43,50 +48,56 @@ export default HomeScreen = ({navigation}) => {
     sqlQuery('SELECT * FROM LocalTable', setItems);
   }, [isVisible]);
 
-  const Item = ({item, onPress, backgroundColor}) => (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[styles.HomeItem, backgroundColor]}>
-      <Text style={[styles.Font, styles.Title]}>{item.name}</Text>
-      <Text style={[styles.Font, styles.Title, styles.Bold]}>
-        {tableSum[item.id - 1]} Ft
-      </Text>
+  const Table = ({item, onPress}) => (
+    <TouchableOpacity style={styles.table} onPress={onPress}>
+      <Text>{item.name}</Text>
+      <Text>{tableSum[item.id - 1]} Ft</Text>
     </TouchableOpacity>
   );
 
-  const renderItem = ({item}) => {
-    const backgroundColor = '#66c2ff';
-
+  const renderTable = ({item}) => {
     return (
-      <Item
+      <Table
         item={item}
         onPress={() => navigation.navigate('TableScreen', {id: item.id})}
-        backgroundColor={{backgroundColor}}
       />
     );
   };
 
   return (
-    <SafeAreaView style={styles.HomeContainer}>
+    <SafeAreaView style={styles.screenContainer}>
       <FlatList
         data={items}
-        ItemSeparatorComponent={listViewItemSeparator}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={renderItem}
+        renderItem={renderTable}
+        keyExtractor={item => item.id}
         numColumns="2"
+        contentContainerStyle={styles.tableList}
       />
-      <View style={{marginBottom: 8, marginTop: 8}}>
-        <Button
-          title="Terméklista"
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
           onPress={() => navigation.navigate('AllProductScreen')}
-        />
-      </View>
-      <View>
-        <Button
-          title="Rendeléstörténet"
+          style={styles.button}>
+          <Text>Terméklista</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
           onPress={() => navigation.navigate('OrderHistory')}
-        />
+          style={styles.button}>
+          <Text>Rendeléstörténet</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    // flex: 1,
+  },
+  tableList: {
+    // alignItems: 'center',
+  },
+  table: {},
+  buttonContainer: {},
+  button: {},
+});
