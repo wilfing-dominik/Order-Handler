98cc788c3b8c9d0ff412395ee6c3c1f8490659f6
wilfing-dominik <wilfing.dominik00@gmail.com>
2023-02-14 14:16:58 +0100

    removed unused import

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import {useIsFocused} from '@react-navigation/native';
 import {
   Text,
-  Button,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
