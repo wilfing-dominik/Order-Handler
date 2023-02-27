f3181fd7b4405605059d45132041c99431692a75
wilfing-dominik <wilfing.dominik00@gmail.com>
2023-02-27 18:06:37 +0100

    restyled modal, added react-native-modal to dependency

--- components/CustomModal.js
+++ components/CustomModal.js
@@ -1,21 +1,17 @@
 import React, {useState} from 'react';
 import {
   Text,
-  SafeAreaView,
   TouchableOpacity,
   View,
-  Modal,
   TextInput,
   StyleSheet,
-  useWindowDimensions,
 } from 'react-native';
-import {sqlQuery} from '../utils/dbConnection';
+import Modal from 'react-native-modal';
 
-const MODAL_HEIGHT = 250;
+import {sqlQuery} from '../utils/dbConnection';
 
 export default CustomModal = ({setAddTableModalOpen, setForceRefresh}) => {
   const [newTableName, setNewTableName] = useState();
-  const {height: screenHeight} = useWindowDimensions();
 
   function handleAddNewTable() {
     sqlQuery('INSERT INTO LocalTable (name) VALUES (?)', null, newTableName);
@@ -24,27 +20,28 @@ export default CustomModal = ({setAddTableModalOpen, setForceRefresh}) => {
   }
 
   return (
-    <Modal transparent={true} animationType="slide">
-      <View
-        style={[
-          styles.addTableModal,
-          {top: (screenHeight - MODAL_HEIGHT) / 2},
-        ]}>
-        <SafeAreaView>
-          <TextInput
-            onChangeText={newTableName => setNewTableName(newTableName)}
-            placeholder={'Asztal neve'}
-            placeholderTextColor="#2b2b28"
-          />
-        </SafeAreaView>
+    <Modal isVisible={true} animationType="fade">
+      <View style={[styles.addTableModal, {top: '15%'}]}>
+        <Text style={styles.title}>Asztal hozzáadása</Text>
 
-        <View>
-          <TouchableOpacity onPress={() => handleAddNewTable()}>
-            <Text>Hozzáadás</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={newTableName => setNewTableName(newTableName)}
+          placeholder={'Asztal neve'}
+          placeholderTextColor="#2b2b28"
+        />
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleAddNewTable()}>
+            <Text style={styles.buttonText}>Hozzáadás</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => setAddTableModalOpen(false)}>
-            <Text>Close</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setAddTableModalOpen(false)}>
+            <Text style={styles.buttonText}>Bezárás</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -54,10 +51,41 @@ export default CustomModal = ({setAddTableModalOpen, setForceRefresh}) => {
 
 const styles = StyleSheet.create({
   addTableModal: {
-    width: 350,
+    borderRadius: 10,
+    width: 325,
+    paddingVertical: 40,
     backgroundColor: '#2b2b28',
-    height: MODAL_HEIGHT,
     position: 'absolute',
     alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: 'black',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: 'white',
+    marginHorizontal: 3,
+    padding: 5,
+    borderRadius: 8,
+    backgroundColor: '#14BCB2',
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '100%',
+    margin: 20,
+    padding: 0,
+    maxWidth: '75%',
+    paddingVertical: 2,
+    paddingHorizontal: 4,
+  },
+  title: {
+    fontSize: 20,
+    color: 'white',
+  },
+  buttonText: {
+    color: '#2b2b28',
+    fontWeight: '500',
   },
 });
